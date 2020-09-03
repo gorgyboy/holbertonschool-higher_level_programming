e6e5a41d862ad407037bcecc1e815420f43646a1
gorgyboy <jlrogb@gmail.com>
2020-09-03 03:40:03 +0000

    Add seventh task

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for j in range(0, 10):
+        if i == 8 and j == 9:
+            print('{:d}{:d}'.format(i, j))
+        elif j > i:
+            print('{:d}{:d}'.format(i, j), end=', ')

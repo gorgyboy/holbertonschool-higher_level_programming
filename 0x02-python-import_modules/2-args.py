29c4b34b51daf4c8bc8c991389409ae5534906d4
gorgyboy <jlrogb@gmail.com>
2020-09-04 00:41:48 +0000

    Add name guard

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 from sys import argv
 arg_len = len(argv)
-if arg_len == 1:
-    print('0 arguments.')
-else:
-    if arg_len == 2:
-        print('1 argument:')
+
+if __name__ == "__main__":
+    if arg_len == 1:
+        print('0 arguments.')
     else:
-        print('{} arguments:'.format(arg_len - 1))
-    for i in range(1, arg_len):
-        print('{}: {}'.format(i, argv[i]))
+        if arg_len == 2:
+            print('1 argument:')
+        else:
+            print('{:d} arguments:'.format(arg_len - 1))
+        for i in range(1, arg_len):
+            print('{:d}: {}'.format(i, argv[i]))

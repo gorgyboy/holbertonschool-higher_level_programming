051cd8873dad314251fee0cc97f8958eae9e1523
gorgyboy <jlrogb@gmail.com>
2020-09-09 01:23:15 +0000

    Fix logic again

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,9 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    tmp_list = my_list
-    tmp_list.reverse()
-    for i in tmp_list:
-        print("{:d}".format(i))
+    if not my_list:
+        pass
+    else:
+        my_list.reverse()
+        for i in tmp_list:
+            print("{:d}".format(i))

6a21e0f490507b3a1e2de7a68aeb341c8df7caca
gorgyboy <jlrogb@gmail.com>
2020-09-09 02:30:19 +0000

    Add sixth task

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def no_c(my_string):
+    new_str = []
+    for c in my_string:
+        if c != 'c' and c != 'C':
+            new_str.append(c)
+    return  "".join(new_str)

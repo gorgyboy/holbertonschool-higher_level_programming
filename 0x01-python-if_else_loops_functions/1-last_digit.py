5ff6388e1a402a744e8f6511144ead9245486964
gorgyboy <jlrogb@gmail.com>
2020-09-14 14:36:55 +0000

    Fix logic when module of a negative number is 0

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,10 +4,13 @@ number = random.randint(-10000, 10000)
 print("Last digit of {:d} is".format(number), end=' ')
 if number >= 0:
     if number % 10 == 0:
-        print("{:d} and is 0".format(number % 10))
+        print("0 and is 0")
     elif number % 10 > 5:
         print("{:d} and is greater than 5".format(number % 10))
     else:
         print("{:d} and is less than 6 and not 0".format(number % 10))
 else:
-    print("{:d} and is less than 6 and not 0".format(-(-number % 10)))
+    if -number % 10 == 0:
+        print("0 and is 0")
+    else:
+        print("{:d} and is less than 6 and not 0".format(-(-number % 10)))

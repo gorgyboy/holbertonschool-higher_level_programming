1cc1d33d99e0ceeedc8a64ae44c800445ce74915
gorgyboy <jlrogb@gmail.com>
2020-09-15 02:19:36 +0000

    FIx logic to get last digit and refactor output

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10000, 10000)
-print("Last digit of {:d} is".format(number), end=' ')
-if number >= 0:
-    if number % 10 == 0:
-        print("{:d} and is 0".format(0))
-    elif number % 10 > 5:
-        print("{:d} and is greater than 5".format(number % 10))
-    else:
-        print("{:d} and is less than 6 and not 0".format(number % 10))
+last = number % (10 if number >= 0 else -10)
+
+print("Last digit of {:d} is {:d} and is".format(number, last), end=' ')
+
+if last == 0:
+    print("0")
+elif last > 5:
+    print("greater than 5")
 else:
-    if -number % 10 == 0:
-        print("{:d} and is 0".format(0))
-    else:
-        print("{:d} and is less than 6 and not 0".format(-(-number % 10)))
+    print("less than 6 and not 0")

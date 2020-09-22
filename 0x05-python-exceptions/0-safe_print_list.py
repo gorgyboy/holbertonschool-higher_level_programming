9c0ac5787f2aa06a01b98a8d8e10a45aa9d5dcff
gorgyboy <jlrogb@gmail.com>
2020-09-21 22:04:26 -0500

    Add check for race conditions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,10 +3,14 @@
 
 def safe_print_list(my_list=[], x=0):
     try:
-        for i in range(0, x):
-            print("{}".format(my_list[i]), end='')
-        print()
-        return i
+        if x == 0:
+            print()
+            return 0
+        else:
+            for i in range(x):
+                print("{}".format(my_list[i]), end='')
+            print()
+            return i + 1
     except:
         print()
         return i

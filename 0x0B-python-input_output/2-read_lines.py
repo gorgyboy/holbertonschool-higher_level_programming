f9e787f6122cc4cea47c7ea67b5c2d7b0c10a026
gorgyboy <jlrogb@gmail.com>
2020-10-07 17:14:18 -0500

    Add 3rd task

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def read_lines(filename="", nb_lines=0):
+    ''' Reads n lines of a text file (UTF8) and prints it to stdout.
+        Reads the entire file if nb_lines is lower or equal to 0
+        OR greater or equal to the total number of lines of the file.
+    '''
+
+    with open(filename, 'r') as f:
+        lines = list(f)
+        if nb_lines <= 0 or nb_lines >= len(lines):
+            for i in range(len(lines)):
+                print(lines[i], end='')
+        else:
+            for i in range(nb_lines):
+                print(lines[i], end='')

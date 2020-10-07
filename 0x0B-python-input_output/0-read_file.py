334c3cae9b2583694df7db96997007c9d89f1425
gorgyboy <jlrogb@gmail.com>
2020-10-07 15:38:10 -0500

    Add 1st task

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def read_file(filename=""):
+    ''' Reads data from file 'filename' '''
+
+    with open(filename, 'r') as f:
+        read_data = f.read()
+        print(read_data, end='')

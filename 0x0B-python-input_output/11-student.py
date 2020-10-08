0d4e9b0a4fefa59f9af8e2c8a82bad40a4b90341
gorgyboy <jlrogb@gmail.com>
2020-10-07 21:23:33 -0500

    Change logic and refactor code

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -3,20 +3,16 @@
 
 
 class Student():
-    """
-    Class representing a student
-    """
+    ''' Class representing a student '''
 
     def __init__(self, first_name, last_name, age):
-        """
-        Initializes a class object
-        """
-        self.first_name = first_name,
+        ''' Initializes an object '''
+
+        self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """
-        Returns a dictionary representation of a class object
-        """
+        ''' Returns a dictionary representation '''
+
         return self.__dict__

9fed0eb709d113e305c65f8e98d42d5930173cf1
gorgyboy <jlrogb@gmail.com>
2020-10-07 21:55:54 -0500

    Add 13th task

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+''' I/O module '''
+
+
+class Student():
+    ''' Class representing a student '''
+
+    def __init__(self, first_name, last_name, age):
+        ''' Initializes an object '''
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        ''' Returns a dictionary representation.
+            If attrs is a list of strings, only attribute names contained in
+            this list must be retrieved. Otherwise, all attributes must be
+            retrieved
+        '''
+
+        if attrs:
+            d_list = self.__dict__
+            d_attrs = dict()
+            for key in attrs:
+                if key in d_list:
+                    d_attrs[key] = d_list[key]
+            return d_attrs
+        else:
+            return self.__dict__

8861eae38786af6b7dc3eb29bcf6745d33758c59
gorgyboy <jlrogb@gmail.com>
2020-10-08 21:29:42 -0500

    Add BaseGeometry class instead of importing it

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python3
 ''' 0x0A-python-inheritance module '''
 
-BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
+
+class BaseGeometry:
+    ''' BaseGeometry class '''
+
+    def area(self):
+        ''' Rises an Exception with the message '''
+
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        ''' Validates value '''
+
+        if type(value) is int:
+            if value <= 0:
+                raise ValueError('{} must be greater than 0'.format(name))
+        else:
+            raise TypeError('{} must be an integer'.format(name))
 
 
 class Rectangle(BaseGeometry):

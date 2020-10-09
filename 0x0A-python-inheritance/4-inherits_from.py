b37e6ef98268f44f66a451b6e4f45cddd4b74d27
gorgyboy <jlrogb@gmail.com>
2020-10-08 20:40:39 -0500

    Add missing type() and make sure obj is a subclass

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,4 +7,4 @@ def inherits_from(obj, a_class):
         (directly or indirectly) from the specified class ; otherwise False.
     '''
 
-    return issubclass(obj, a_class)
+    return issubclass(type(obj), a_class) and type(obj) is not a_class

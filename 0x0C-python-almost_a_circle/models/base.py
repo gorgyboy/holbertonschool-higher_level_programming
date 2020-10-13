c27c0326427924eeb617cdc49c40fbd5e1e66c04
gorgyboy <jlrogb@gmail.com>
2020-10-12 21:00:13 -0500

    Add class method save_to_file for Base class and add missing decorator

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,8 +28,10 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """ Returns the JSON string representation of list_dictionaries.
+
             Attributes:
                 list_dictionaries (list, dict): List of dictionaries to export
                                                 to JSON string.
@@ -38,3 +40,16 @@ class Base:
             return dumps(list_dictionaries)
         else:
             return '"[]"'
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ Writes the JSON string representation of list_objs to a file.
+
+            Attributes:
+                list_objs (list): List of instances who inherits of Base.
+        """
+        with open(cls.__name__ + '.json', 'w', encoding='utf-8') as f:
+            dic = []
+            for obj in list_objs:
+                dic.append(obj.to_dictionary())
+            f.write(Base.to_json_string(dic))

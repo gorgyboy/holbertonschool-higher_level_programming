e7897ddbfda927c706ab59662bd409cdb5f82d11
gorgyboy <jlrogb@gmail.com>
2020-12-03 02:50:54 +0000

    Add task 3

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+print_square module
+"""
+
+
+def print_square(size):
+    """
+    Prints a square with the character #
+    """
+
+    if type(size) is not int:
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise TypeError('size must be >= 0')
+
+    for row in range(size):
+        for col in range(size):
+            print('#', end='')
+        print()

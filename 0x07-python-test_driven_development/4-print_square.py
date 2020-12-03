b00358a7ab6181b6d36fff70fec31201ddfd8107
gorgyboy <jlrogb@gmail.com>
2020-12-03 03:04:12 +0000

    Refactor code

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -9,12 +9,10 @@ def print_square(size):
     Prints a square with the character #
     """
 
-    if type(size) is not int:
+    if not isinstance(size, int):
         raise TypeError('size must be an integer')
     if size < 0:
         raise TypeError('size must be >= 0')
 
     for row in range(size):
-        for col in range(size):
-            print('#', end='')
-        print()
+        print('#' * size)

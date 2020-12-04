96880bcb75670aac6167c3e74d42235ee225418c
gorgyboy <jlrogb@gmail.com>
2020-12-04 04:25:41 -0500

    Add docstring

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+LockedClass module
+"""
+
 
 class LockedClass():
+    """
+    Low memory cost class with only <first_name> attribute
+    """
+
     __slots__ = 'first_name'

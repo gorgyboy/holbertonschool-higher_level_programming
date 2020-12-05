339e7f2b34f6f9cebe4263c3864640900dad1f3d
gorgyboy <jlrogb@gmail.com>
2020-12-04 19:48:24 -0500

    Add task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+Rectangle module
+"""
+
+
+class Rectangle():
+    """
+    Recatangle class
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        Initializes a new instance.
+        Args:
+            width (int): Rectangle's width.
+            height (int): Rectangle's height.
+        """
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """
+        Sets and returns width
+        """
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Verifies that value is int and greater than 0
+        """
+
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        Sets and returns height
+        """
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Verifies that value is int and greater than 0
+        """
+
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value <= 0:
+            raise ValueError('height must be > 0')
+        self.__height = value

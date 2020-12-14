b74702f69b8fbf4ac03c443c0eb52817849b827d
gorgyboy <jlrogb@gmail.com>
2020-12-14 03:17:48 -0500

    Add task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""PYTHON OBJECT RELATIONAL MAPPING MODULE"""
+
+import MySQLdb
+from sys import argv
+
+
+def my_safe_filter_states():
+    """
+    Takes in an argument and displays all values in the states table of the
+    database where name matches the argument. Is safe from MySQL injections!
+
+    Recieves 4 arguments: mysql username, mysql password, database name and
+    state name searched.
+    """
+
+    if len(argv[4].split()) == 1:
+        conn = MySQLdb.connect(host="localhost", port=3306,
+                               user=argv[1], passwd=argv[2], db=argv[3],
+                               charset="utf8")
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM states WHERE name LIKE '" +
+                    argv[4] + "' ORDER BY id ASC")
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+        cur.close()
+        conn.close()
+
+
+if __name__ == "__main__":
+    my_safe_filter_states()

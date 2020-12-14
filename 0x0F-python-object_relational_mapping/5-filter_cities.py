f3e24d3f7927b22d61affa95fe9697811354ad3c
gorgyboy <jlrogb@gmail.com>
2020-12-14 04:12:56 -0500

    Add task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""PYTHON OBJECT RELATIONAL MAPPING MODULE"""
+
+import MySQLdb
+from sys import argv
+
+
+def filter_cities():
+    """
+    Takes in the name of a state as an argument and lists all cities of that
+    state, using the database.
+
+    Takes  4 arguments: mysql username, mysql password, database name and
+    state name (SQL injection free!).
+    """
+
+    if len(argv[4].split()) == 1:
+        conn = MySQLdb.connect(host="localhost", port=3306,
+                               user=argv[1], passwd=argv[2], db=argv[3],
+                               charset="utf8")
+        cur = conn.cursor()
+        cur.execute("SELECT name " +
+                    "FROM cities " +
+                    "WHERE state_id=(SELECT id FROM states WHERE name LIKE '" +
+                    argv[4] + "') " +
+                    "ORDER BY id ASC")
+        query_rows = cur.fetchall()
+        for i in range(len(query_rows)):
+            print(query_rows[i][0], end='')
+            if i < len(query_rows) - 1:
+                print(',', end=' ')
+        print()
+        cur.close()
+        conn.close()
+
+
+if __name__ == "__main__":
+    filter_cities()

19f32a621fffc02ae68bcdb29800657b2a10a70f
gorgyboy <jlrogb@gmail.com>
2020-12-14 02:27:04 -0500

    Add task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""PYTHON OBJECT RELATIONAL MAPPING MODULE"""
+
+import MySQLdb
+from sys import argv
+
+
+def my_filter_states():
+    """
+    Takes in an argument and displays all values in the states table of the
+    database where name matches the argument.
+
+    Recieves 4 arguments: mysql username, mysql password, database name and
+    state name searched.
+    """
+
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=argv[1], passwd=argv[2], db=argv[3],
+                           charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE '" +
+                str(argv[4]) + "' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    my_filter_states()

9b5d3e28be8410d4445df5a4d8ab19a787d1b481
gorgyboy <jlrogb@gmail.com>
2020-12-16 01:10:43 -0500

    Add task 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+PYTHON OBJECT RELATIONAL MAPPING MODULE
+
+model_state_filter_a module provides function to get that contain the letter
+'a' from the states table in the DB.
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+
+
+def model_state_filter_a():
+    """
+    Lists all State objects that contain the letter a from the database.
+
+    Takes 3 arguments: mysql username, mysql password and database name.
+    """
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for row in session.query(State).filter(text("name LIKE BINARY '%a%'"))\
+            .order_by(State.id.asc()):
+        print('{}: {}'.format(row.id, row.name))
+
+
+if __name__ == "__main__":
+    model_state_filter_a()

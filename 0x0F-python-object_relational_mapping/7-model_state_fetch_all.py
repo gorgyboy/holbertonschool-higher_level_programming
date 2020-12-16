54d14b6fe4b69b45ba78c78baa70177816602610
gorgyboy <jlrogb@gmail.com>
2020-12-15 23:58:01 -0500

    Add task 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+PYTHON OBJECT RELATIONAL MAPPING MODULE
+
+Model_State_Fetch_All module provides function to get all states from states
+table in the DB.
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def model_state_fetch_all():
+    """
+    Lists all State objects from the database and show results sorted in 
+    ascending order by states.id.
+
+    Takes 3 arguments: mysql username, mysql password and database name.
+    """
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for row in session.query(State).order_by(State.id.asc()):
+        print('{}: {}'.format(row.id, row.name))
+
+
+if __name__ == "__main__":
+    model_state_fetch_all()

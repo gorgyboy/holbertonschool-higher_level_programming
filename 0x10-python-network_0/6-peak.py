dae8bcaf5218bea92c249b83cb81ca85672e5c81
gorgyboy <jlrogb@gmail.com>
2021-01-31 16:21:38 -0500

    Add task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+def find_peak(list_of_integers):
+    '''Finds a peak in a list of unsorted integers'''
+
+    if len(list_of_integers) == 0:
+        return None
+    elif len(list_of_integers) == 1:
+        return list_of_integers[0]
+
+    peak = list_of_integers[0]
+
+    for i in range(len(list_of_integers)):
+        if i == 0:
+            if list_of_integers[i] >= list_of_integers[i+1]:
+                peak = list_of_integers[i]
+        else:
+            if i < len(list_of_integers)-1:
+                if list_of_integers[i] >= all([list_of_integers[i-1],
+                                               list_of_integers[i+1]]):
+                    if list_of_integers[i] > peak:
+                        peak = list_of_integers[i]
+
+            elif list_of_integers[i] >= list_of_integers[i-1]:
+                if list_of_integers[i] > peak:
+                    peak = list_of_integers[i]
+    return peak

c01532dfd29f61ace5fa0a9aff540422a96add9a
gorgyboy <jlrogb@gmail.com>
2021-02-01 00:39:31 -0500

    Refactor code to make it more performant

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,29 +1,39 @@
 #!/usr/bin/python3
-'''Python Network 0 module'''
+'''Python Network 0 module - Task 6'''
 
 
 def find_peak(list_of_integers):
-    '''Finds a peak in a list of unsorted integers'''
+    '''Wrapper method to find a peak in a list of unsorted integers'''
 
-    if len(list_of_integers) == 0:
+    size = len(list_of_integers)
+
+    if size == 0:
         return None
-    elif len(list_of_integers) == 1:
+    elif size == 1:
         return list_of_integers[0]
+    return search_list_for_peak(list_of_integers, 0, size - 1, size)
 
-    peak = list_of_integers[0]
 
-    for i in range(len(list_of_integers)):
-        if i == 0:
-            if list_of_integers[i] >= list_of_integers[i+1]:
-                peak = list_of_integers[i]
-        else:
-            if i < len(list_of_integers)-1:
-                if list_of_integers[i] >= all([list_of_integers[i-1],
-                                               list_of_integers[i+1]]):
-                    if list_of_integers[i] > peak:
-                        peak = list_of_integers[i]
+def search_list_for_peak(list_int, bottom, top, size):
+    '''Finds and returns ta peak element using an adapted binary search'''
 
-            elif list_of_integers[i] >= list_of_integers[i-1]:
-                if list_of_integers[i] > peak:
-                    peak = list_of_integers[i]
-    return peak
+    middle = int((bottom + top) / 2)
+
+    # if middle is 0, then middle - 1 is false, so we're at the beginning.
+    # if middle is size - 1, then middle + 1 is false, so we're at the end.
+    # if we're at either end, we check for peak.
+    # else, if we're not at either end, then we check if middle is peak with
+    # the list.
+    if (middle == 0 or list_int[middle] >= list_int[middle - 1]) and \
+            (middle == size - 1 or list_int[middle] >= list_int[middle + 1]):
+        return list_int[middle]
+
+    # if we didn't find the peak, we check if the left neighbor is greater than
+    # middle.
+    # if it is, then we search for the peak in the left side of the list.
+    elif middle > 0 and list_int[middle] < list_int[middle - 1]:
+        return search_list_for_peak(list_int, bottom, middle - 1, size)
+
+    # otherwise, we search for the peak in the right side of the list.
+    else:
+        return search_list_for_peak(list_int, middle + 1, top, size)

a22d886f09d5d5cb505f9a69bae490128a10f2ae
gorgyboy <jlrogb@gmail.com>
2021-02-02 16:07:34 -0500

    Add task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Prints 3 lines: "C is fun", "Python is cool", "Javascript is amazing"
+
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');

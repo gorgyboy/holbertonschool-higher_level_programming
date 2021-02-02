1ca9a2eaed5cdb16735171c47de2aa5faa44ff92
gorgyboy <jlrogb@gmail.com>
2021-02-02 17:22:28 -0500

    Add task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Prints "C is fun", "Python is cool", "Javascript is amazing" using a loop
+
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+array.forEach(element => console.log(element));

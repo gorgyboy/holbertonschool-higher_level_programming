be97a9752bc174c84a0893a8145bbbe5484ef1ca
gorgyboy <jlrogb@gmail.com>
2021-02-02 19:39:35 -0500

    Refactor code to fix bigger not being updated.
    With the new logic, bigger will be assinged in the first iteration and
    bigger will be check even if biggest doesn't change, just to make sure
    that is the correct second biggest number.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,20 +2,22 @@
 // Searches the second biggest integer in the list of arguments
 
 const size = process.argv.length;
-let bigger, biggest;
+let bigger, biggest, num;
 let index;
 
 if (size <= 3) {
   console.log(0);
 } else {
-  bigger = parseInt(process.argv[2]);
   biggest = parseInt(process.argv[2]);
   // We have at least to args, so we start from the second, becasue the first
-  // is bigger and biggest
+  // is biggest
   for (index = 3; index < size; index++) {
-    if (process.argv[index] > biggest) {
+    num = parseInt(process.argv[index]);
+    if (num > biggest) {
       bigger = biggest;
-      biggest = parseInt(process.argv[index]);
+      biggest = num;
+    } else if (bigger === undefined || num > bigger) {
+      bigger = num;
     }
   }
   console.log(bigger);

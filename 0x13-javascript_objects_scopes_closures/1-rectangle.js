3abd73bdfe4ffbd4ea2a2bbfdc5817c0c17e9423
gorgyboy <jlrogb@gmail.com>
2021-02-08 15:44:34 -0500

    Add task 1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// Defines a empty class Rectangle.
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+/*
+Do not forget to type it as "module.exports", otherwise it doesn't work
+when called by require. "module" refers to the current module, which is the
+present script and this is like this becuase Node.js conciders each script as
+module, so when using "exports", "module" refers to the current script,
+as said before.
+*/
+module.exports = Rectangle;

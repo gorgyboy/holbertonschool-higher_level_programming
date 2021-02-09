a7b42668a5513bea5ef4c04a26bdfaf25f525b4e
gorgyboy <jlrogb@gmail.com>
2021-02-08 22:40:53 -0500

    Fix comments

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-
 // Returns the number of occurrences of searchElement in a list.
+
 exports.nbOccurences = function (list, searchElement) {
   let nb = 0;
   for (const element of list) {
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-
 // Returns the reversed version of a list.
+
 exports.esrever = function (list) {
   const revList = [];
   for (let revIndex = list.length - 1; revIndex >= 0; revIndex--) {

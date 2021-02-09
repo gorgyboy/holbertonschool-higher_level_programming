59ff7d70f050177a6a2d0122e3c6fe64231ef603
gorgyboy <jlrogb@gmail.com>
2021-02-09 14:14:38 -0500

    Add Adv. task 12

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,4 +2,17 @@
 // Imports a dictionary of occurrences by user id and computes a dictionary
 // of user ids by occurrence.
 
-// const dict = require('./101-data.js').dict;
+const dict = require('./101-data.js').dict;
+let newDict = new Object();
+
+for (const key in dict) {
+  if (!(newDict.hasOwnProperty(key))) {
+    newDict[dict[key]] = [];
+  }
+}
+
+for (const key in dict) {
+  newDict[dict[key]].push(key);
+}
+
+console.log(newDict);

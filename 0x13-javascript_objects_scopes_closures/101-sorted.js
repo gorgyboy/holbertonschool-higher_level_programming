598033b045b85cdc23d4091898b17424c0fcaff4
gorgyboy <jlrogb@gmail.com>
2021-02-09 14:25:29 -0500

    Refactor script

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -6,12 +6,9 @@ const dict = require('./101-data.js').dict;
 const newDict = {};
 
 for (const key in dict) {
-  if (!(key in newDict)) {
+  if (!(dict[key] in newDict)) {
     newDict[dict[key]] = [];
   }
-}
-
-for (const key in dict) {
   newDict[dict[key]].push(key);
 }
 

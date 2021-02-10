2ce448d559d737e4ad2a0639dc5bdfba18fdb93e
gorgyboy <jlrogb@gmail.com>
2021-02-09 23:41:24 -0500

    Add Adv. task 13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+// Concats 2 files.
+// The first argument is the file path of the first source file.
+// The second argument is the file path of the second source file.
+// The third argument is the file path of the destination.
+
+const fs = require('fs');
+const f1 = fs.readFileSync(process.argv[2], 'utf8');
+const f2 = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], f1 + f2, 'utf-8');

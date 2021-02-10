a201f99b7e8519c5bdd1f6ce04d8e32af28364c9
gorgyboy <jlrogb@gmail.com>
2021-02-10 12:06:37 -0500

    Add task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
+// Reads and prints the content of a file.
+
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', function (error, data) {
   console.log(error || data);

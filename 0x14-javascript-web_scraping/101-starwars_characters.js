2fede731564b6f8b2ac9a25226d2c2d4a8965ca3
gorgyboy <jlrogb@gmail.com>
2021-02-10 23:51:03 -0500

    Add task 8 - Adv.

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin / node
+// Prints all characters of a Star Wars movie. Displays one character name by
+// line in the same order of the list “characters” in the /films/ response.
+
+const request = require('request');
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
+const orderedChars = {};
+request(url, (error, response, content) => {
+  if (!error) {
+    const people = JSON.parse(content).characters;
+    people.forEach((character) => {
+      request(character, (error, response, content) => {
+        if (!error) {
+          orderedChars[character.split('/')[5]] = JSON.parse(content).name;
+          if (Object.entries(orderedChars).length === people.length) {
+            Object.values(orderedChars).forEach((element) => {
+              console.log(element);
+            });
+          }
+        }
+      });
+    });
+  }
+});

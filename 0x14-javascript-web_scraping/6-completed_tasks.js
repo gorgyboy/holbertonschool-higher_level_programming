fa53fb0969afdaeba3d8cb0e56a0b9c1a0ad0871
gorgyboy <jlrogb@gmail.com>
2021-02-10 23:43:51 -0500

    Add task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// Computes the number of tasks completed by user id.
+
+const request = require('request');
+request(process.argv[2], (error, response, body) => {
+  if (!error) {
+    const todos = JSON.parse(body);
+    const done = {};
+    todos.forEach((todo) => {
+      if (done[todo.userId] === undefined && todo.completed) {
+        done[todo.userId] = 1;
+      } else if (todo.completed) {
+        done[todo.userId] += 1;
+      }
+    });
+    console.log(done);
+  }
+});
